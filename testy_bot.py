79eca5a34dfe5a249bc130d257bc1f0d88cd01dc
Kenny Wu <KennyWu@github.com>
2022-11-07 11:21:06 -0500

    working with new token

--- testy_bot.py
+++ testy_bot.py
@@ -2,15 +2,19 @@
 
 import discord
 
+
 class MyClient(discord.Client):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
 
-client.run('OTMxNDIyNzUxMzA5MzMyNTYw.GuGyGD.7NlKK32N_jSJOLb9_G8Pqp5_Thz2Dofih0feME')
+client.run('OTMxNDIyNzUxMzA5MzMyNTYw.GNOTJH.6vG18RHSbNRTrG-c8mZr29OuhYcsUnHSFQdoQs')
