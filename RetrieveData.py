9cb37a62fdf0fbe2ec562d31823431f5fdeaa4e7
Kenny Wu <43484854+KennyWu@users.noreply.github.com>
2022-11-14 22:55:20 -0500

    added more unit testing/helper method

--- RetrieveData.py
+++ RetrieveData.py
@@ -16,56 +16,83 @@ from bs4 import BeautifulSoup
 #     #     print('open seats ' + sec.find('span', class_="open-seats-count").text)
 #     #     print('waitlist ' + sec.find('span', class_="waitlist-count").text)
 
-    #TODO - please add testing holy shit add testing i wrote too much code i dont know if it works lmao
+# TODO - please add testing holy shit add testing i wrote too much code i dont know if it works lmao
 
 class Retrieve:
-    
-    def __init__(self, course_selection:dict = None) :
-         self._course_selection = course_selection
-    
+
+    def __init__(self, course_selection: dict = None, semester: str = None):
+        self._course_selection = course_selection
+        self._semester = semester
+
+    @property
+    def semester(self):
+        return self._semester
+
+    @semester.setter
+    def semester(self, semester: str):
+        self._semester = semester
+
     @property
     def course_selection(self):
         return self._course_selection
-    
+
     @course_selection.setter
-    def course_selection(self, d:dict):
+    def course_selection(self, d: dict):
         self._course_selection = d
-        
-    def add(self, course, section = None):
+
+    def delete(course, Section=None):
+        raise Exception
+
+    def add(self, course: str, section: set = None):
         success = False
         if (section != None):
-            if (self._course_selection.has_key(course)):
-                self._course_selection[course].extend[section]
+            if course in self._course_selection:
+                self._course_selection[course] = self._course_selection[course].union(
+                    section)
                 success = True
-            else:
+            elif self._verify(course):
                 self._course_selection[course] = section
                 success = True
         return success
-    
-    
-    def delete(course, Section = None):
-        raise Exception
-    
-#To do add testing perferably unit testing, however since small project might be more efficent to do print
+
+    # TODO add section verification
+    def __verify(self, course: str, section: set = None):
+        response = requests.get("""https://app.testudo.umd.edu/soc/search?courseId={}
+        &sectionId=&termId={}&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=AL
+        L&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&
+        courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&
+        _classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on""",
+                                self._course_selection, self._semester)
+        soup = BeautifulSoup(response, 'lxml')
+        return soup.find('div', id=course) != None
+
+    # TODO conduct unit testing, change url
+
     def retrieve(self):
         if (self._course_selection != None):
-            response = requests.get("https://app.testudo.umd.edu/soc/search?courseId=CMSC216&sectionId=&termId=202301&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on")
+            response = requests.get("""https://app.testudo.umd.edu/soc/search?
+            courseId=CMSC216&sectionId=&termId=202301&_openSectionsOnly=on&
+            creditCompare=&credits=&courseLevelFilter=ALL&instructor=&
+            _facetoface=on&_blended=on&_online=on&courseStartCompare=&
+            courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&
+            courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&
+            _classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on""")
             soup = BeautifulSoup(response.content, 'lxml')
             for course_name in self._course_selection.keys():
                 div = soup.find('div', id=course_name)
                 if (div != None):
-                    new_div = div.find_all('div', class_='section-info-container')
+                    new_div = div.find_all(
+                        'div', class_='section-info-container')
                     for section in self._course_selection[course_name]:
                         for section_html in new_div:
                             if section in section_html.find('span').text:
                                 print('Course' + course_name)
                                 print('section' + section)
-                                print('open seats ' + sec.find('span', class_="open-seats-count").text)
-                                print('waitlist ' + sec.find('span', class_="waitlist-count").text)     
+                                print('open seats ' + sec.find('span',
+                                      class_="open-seats-count").text)
+                                print('waitlist ' + sec.find('span',
+                                      class_="waitlist-count").text)
                 else:
                     raise BaseException().add_note('Could not retrieve html data for class')
         else:
             return False
-            
-                
-            
--- _test.py
+++ _test.py
@@ -1,21 +1,45 @@
 import pytest
 from RetrieveData import Retrieve
 
+
 def test_one():
     """Tests the dictionary values is None with newly initalized Retrieve Object"""
     retrieve = Retrieve()
     assert retrieve.course_selection == None
-    
+
+
 def test_two():
     """Test that the retrieve object will hold the correct dictionary values when updated"""
-    a = True
-    assert a == True
-    
-def test_three():
-    a = True
-    assert a == True
-    
-def test_four():
-    a = True
-    assert a == True
+    test_dict = {'CMSC216': {110, 120, 130, 140},
+                 'CMSC132': {110, 120, 130, 140}}
+    retrieve = Retrieve()
+    retrieve.course_selection = test_dict
+    assert retrieve.course_selection == {'CMSC216': {
+        110, 120, 130, 140}, 'CMSC132': {110, 120, 130, 140}}
 
+
+def test_three():
+    """Test adding new key value pair to the dictionary"""
+    test_dict = {'CMSC216': {110, 120, 130, 140},
+                 'CMSC132': {110, 120, 130, 140}}
+    retrieve = Retrieve()
+    retrieve.course_selection = test_dict
+    assert retrieve.add(course='CMSC417') == False
+    assert retrieve.add(course='CMSC417', section={100, 200, 300, 400}) == True
+    assert retrieve.course_selection == {'CMSC216': {110, 120, 130, 140}, 'CMSC132': {
+        110, 120, 130, 140}, 'CMSC417': {100, 200, 300, 400}}
+
+
+def test_four():
+    """Tests adding new sections to existing classes"""
+    test_dict = {'CMSC216': {110, 120, 130, 140},
+                 'CMSC132': {110, 120, 130, 140}}
+    retrieve = Retrieve()
+    retrieve.course_selection = test_dict
+    assert retrieve.add(course='CMSC216', section={110, 150, 160})
+    assert retrieve.course_selection == {'CMSC216': {110, 120, 130, 140, 150, 160},
+                                         'CMSC132': {110, 120, 130, 140}}
+
+
+def test_five():
+    """Tests for if a class doesn't exist"""
