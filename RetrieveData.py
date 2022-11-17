b2e82f8581868e46ccdce98d887c51201f552d4d
KennyWu@github.com <43484854+KennyWu@users.noreply.github.com>
2022-11-17 13:41:51 -0500

    added more verification and delete method

--- RetrieveData.py
+++ RetrieveData.py
@@ -7,14 +7,12 @@ from bs4 import BeautifulSoup
 # # print(response.text)
 # soup = BeautifulSoup(response.content, "lxml")
 # divs = soup.find('div', id='CMSC216')
-# print(divs)
+# print(divs.find('input', value="0"))
 # new_div = divs.find_all('div', class_='section-info-container')
-# # print(divs.prettify)
 # for sec in new_div:
-#     print(sec)
-#     # if "0101" in sec.find('span').text:
-#     #     print('open seats ' + sec.find('span', class_="open-seats-count").text)
-#     #     print('waitlist ' + sec.find('span', class_="waitlist-count").text)
+#     if "0101" in sec.find('span').text:
+#         print('open seats ' + sec.find('span', class_="open-seats-count").text)
+#         print('waitlist ' + sec.find('span', class_="waitlist-count").text)
 
 # TODO - please add testing holy shit add testing i wrote too much code i dont know if it works lmao
 
@@ -41,33 +39,63 @@ class Retrieve:
     def course_selection(self, d: dict):
         self._course_selection = d
 
-    def delete(course, Section=None):
-        raise Exception
+    #TODO - add unit testing for delete
+    def delete(self, course, section: set=None):
+        """Deletes the course or the specific sections of courses
+            - If section is not part of the existing set then nothing happens
+            - Returns True if course if found and deleted
+            - Returns False otherwise
+        """
+        success = False
+        if course in self._course_selection:
+            success = True
+            if (section != None):
+                for sec in section:
+                    if sec in self.course_selection[course]:
+                      self.course_selection[course].remove(sec)  
+            else:
+                self._course_selection.pop(course, None)
+        return success  
+    
+    def __check_semster(self):
+        if (self._semester == None):
+            raise Exception("No semester added")
 
     def add(self, course: str, section: set = None):
+        self.__check_semster()
         success = False
         if (section != None):
             if course in self._course_selection:
-                self._course_selection[course] = self._course_selection[course].union(
-                    section)
-                success = True
-            elif self.__verify(course):
+                if self.__verify(course, section=section) :
+                    self._course_selection[course] = self._course_selection[course].union(
+                        section)
+                    success = True
+            elif self.__verify(course, section=section):
                 self._course_selection[course] = section
                 success = True
         return success
 
     # TODO add section verification
-    def __verify(self, course: str, section: set = None):
+    def __verify(self, course: str, section: set):
         url = f"https://app.testudo.umd.edu/soc/search?courseId={course}&sectionId=&termId={self._semester}&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
         response = requests.get(url)
-        if response.status_code == 400:
-            return False
-        soup = BeautifulSoup(response.content, 'lxml')
-        return soup.find('div', id=course) != None
+        success = False
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'lxml')
+            course_html = soup.find('div', id=course)
+            if (course_html != None):
+                for sec in section:
+                    if soup.find('input', value=sec) != None:
+                        success = True
+                    else:
+                        success = False
+                        break
+        return success
 
     # TODO conduct unit testing, change url
 
     def retrieve(self):
+        self.__check_semster()
         if (self._course_selection != None):
             response = requests.get("https://app.testudo.umd.edu/soc/search?courseId=CMSC216&sectionId=&termId=202301&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on")
             soup = BeautifulSoup(response.content, 'lxml')
--- _test.py
+++ _test.py
@@ -25,19 +25,19 @@ def test_three():
     retrieve = Retrieve(semester=202301)
     retrieve.course_selection = test_dict
     assert retrieve.add(course='CMSC417') == False
-    assert retrieve.add(course='CMSC417', section={100, 200, 300, 400}) == True
+    assert retrieve.add(course='CMSC417', section={"0101"}) == True
     assert retrieve.course_selection == {'CMSC216': {110, 120, 130, 140}, 'CMSC132': {
-        110, 120, 130, 140}, 'CMSC417': {100, 200, 300, 400}}
+        110, 120, 130, 140}, 'CMSC417': {"0101"}}
 
 
 def test_four():
     """Tests adding new sections to existing classes"""
-    test_dict = {'CMSC216': {110, 120, 130, 140},
+    test_dict = {'CMSC216': {'0101', '0102', '0103'},
                  'CMSC132': {110, 120, 130, 140}}
-    retrieve = Retrieve()
+    retrieve = Retrieve(semester=202301)
     retrieve.course_selection = test_dict
-    assert retrieve.add(course='CMSC216', section={110, 150, 160})
-    assert retrieve.course_selection == {'CMSC216': {110, 120, 130, 140, 150, 160},
+    assert retrieve.add(course='CMSC216', section={'0104', '0105'}) == True
+    assert retrieve.course_selection == {'CMSC216': {'0101', '0102', '0103', '0104', '0105'},
                                          'CMSC132': {110, 120, 130, 140}}
 
 
@@ -48,3 +48,11 @@ def test_five():
     retrieve = Retrieve(semester=202301)
     retrieve.course_selection = test_dict
     assert retrieve.add(course='CS0-1', section={100, 200, 300, 400}) == False
+    
+def test_six():
+    """Tests for invalid sections"""
+    test_dict = {'CMSC216': {110, 120, 130, 140},
+                 'CMSC132': {110, 120, 130, 140}}
+    retrieve = Retrieve(semester=202301)
+    retrieve.course_selection = test_dict
+    assert retrieve.add(course='CMSC216', section={'0000', '0101'}) == False
